72b7061cc17dd4780274a96911dc94e22913fd4e
JoelAdj01 <kofienergy15@gmail.com>
2025-04-01 13:59:41 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,26 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: [
-      '@solana/web3.js',
-      'react-toastify',
-      '@solana/wallet-adapter-wallets',
+        '@solana/web3.js',
+        'react-toastify',
+        '@solana/wallet-adapter-wallets',
       '@solana/wallet-adapter-react',
       '@solana/wallet-adapter-base',
       '@solana/wallet-adapter-react-ui',
       '@solana/spl-token'
-    ],
+      ],
   },
-  // Remove the external configuration since these are core dependencies
-  // that should be bundled with your application
   build: {
-    // Either remove this section entirely or modify it to exclude only truly external resources
-    // rollupOptions: {
-    //   external: [
-    //     // Remove packages that you actually need bundled
-    //   ],
-    // },
+    rollupOptions: {
+      external: [
+        '@solana/web3.js',
+        'react-toastify',
+        '@solana/wallet-adapter-wallets',
+        '@solana/wallet-adapter-react',
+      '@solana/wallet-adapter-base',
+      '@solana/wallet-adapter-react-ui',
+      '@solana/spl-token'
+      ],
+    },
   },
 })
