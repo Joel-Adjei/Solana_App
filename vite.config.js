833a10df235c0e568a7db6edc76c967a47321051
JoelAdj01 <kofienergy15@gmail.com>
2025-04-01 04:06:02 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Solana_App"
+  base: process.env.VITE_BASE_PATH || "/Solana_App",
+  optimizeDeps: {
+    include: ['@solana/web3.js'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@solana/web3.js'],
+    },
+  },
 })
