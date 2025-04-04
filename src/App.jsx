67f9c8a9aa6556d0142d253083af2f11d112e9bd
JoelAdj01 <kofienergy15@gmail.com>
2025-04-04 04:35:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './App.css';
 import WalletConnection from './components/WalletConnection';
 import UserActions from './components/UserActions'
